1c25e630f3ec34a526c2c6de589d7345a7fd6c37
jbac208 <jbac208@aucklanduni.ac.nz>
2023-05-05 16:13:30 +1200

    iterating and printing round number

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -4,13 +4,19 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
 
+  // Global variables
+  private int roundCount = 0;
+
   public Morra() {}
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    roundCount++;
+    MessageCli.START_ROUND.printMessage(String.valueOf(roundCount));
+  }
 
   public void showStats() {}
 }
