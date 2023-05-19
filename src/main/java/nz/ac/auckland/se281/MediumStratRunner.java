3acdd604d4e4d7250f95d437c41e4909032b9915
jbac208 <jbac208@aucklanduni.ac.nz>
2023-05-19 17:13:44 +1200

    implemented MediumStratRunner functinoalites. need to pass in params now

--- src/main/java/nz/ac/auckland/se281/MediumStratRunner.java
+++ src/main/java/nz/ac/auckland/se281/MediumStratRunner.java
@@ -2,21 +2,33 @@ package nz.ac.auckland.se281;
 
 public class MediumStratRunner implements StratRunner {
 
+  private int roundCount;
+  private int playerTotalFingers;
+  private Game strat = new Game(new RandomStrategy()); // initially use random strat
+
+  public MediumStratRunner(int roundCount, int playerTotalFingers) {
+    this.roundCount = roundCount;
+    this.playerTotalFingers = playerTotalFingers;
+  }
+
   @Override
   public void runStrat() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'runStrat'");
+    if (roundCount <= 3) {
+      // for first 3 rounds use random strat
+      strat.chooseFingersAndSum();
+    } else {
+      // from 4th round onwards use avg strat
+      strat.setStrategy(new AverageStrategy(roundCount, playerTotalFingers)); // pass params
+    }
   }
 
   @Override
   public int getFingers() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getFingers'");
+    return strat.getFingers();
   }
 
   @Override
   public int getSum() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getSum'");
+    return strat.getSum();
   }
 }
